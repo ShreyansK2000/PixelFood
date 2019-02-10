c4042cba0dd709ab0e042665be7b6dfd160e94a9
Yash_Sardhara <yash.sardhara1@gmail.com>
2019-02-10 03:05:34 -0800

    Server Interaction

--- PixelFood/app/src/main/java/com/example/pixelfoodapp/MainActivity.java
+++ PixelFood/app/src/main/java/com/example/pixelfoodapp/MainActivity.java
@@ -2,27 +2,41 @@ package com.example.pixelfoodapp;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NavUtils;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.UUID;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
 
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
+    private static final String Upload_URL = "http://127.0.0.1:8000/";
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+        requestStoragePermission();
+        //ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2);
         Button btnCamera = (Button)findViewById(R.id.btnCamera);
+        Button btnUpload = (Button)findViewById(R.id.btnUpload);
         imageView = (ImageView)findViewById(R.id.imageView);
 
         btnCamera.setOnClickListener(new View.OnClickListener() {
@@ -40,42 +57,70 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 3);
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
-       super.onActivityResult(requestCode, resultCode, data);
-       // Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        Bundle bundle = data.getExtras();
-        Bitmap bitmap = (Bitmap) bundle.get("data");
+        if(requestCode == 0){
+            super.onActivityResult(requestCode, resultCode, data);
+            // Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
 
 
-       // String NameOfFolder = "FoodPixel";
-        String NameOfFile = "FoodPixelFood";
-        //String file_path = Environment.getExternalStorageDirectory().getAbsolutePath()+NameOfFolder;
-        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        String currentDateAndTime = getCurrentDateAndTime();
-        //File directory = new File(file_path);
+            // String NameOfFolder = "FoodPixel";
+            String NameOfFile = "FoodPixelFood";
+            //String file_path = Environment.getExternalStorageDirectory().getAbsolutePath()+NameOfFolder;
+            File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+            String currentDateAndTime = getCurrentDateAndTime();
+            //File directory = new File(file_path);
 
 //                if(!directory.exists()){
 //                    directory.mkdirs();
 //                }
 
-        File imFile = new File(pictureDirectory, NameOfFile + currentDateAndTime + ".png");
-        // Uri pictureUri = Uri.fromFile(imFile);
-        // intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
+            File imFile = new File(pictureDirectory, NameOfFile + currentDateAndTime + ".png");
 
-        OutputStream out = null;
+            Uri pictureUri = Uri.fromFile(imFile);
+            // intent.putExtra(MediaStore.EXTRA_OUTPUT, pictureUri);
 
-        try{
-            out = new FileOutputStream(imFile);
-            bitmap.compress(Bitmap.CompressFormat.PNG,100,out);
-            out.flush();
-            out.close();
-        } catch (java.io.IOException e){
-            e.printStackTrace();
+            OutputStream out = null;
+
+            try{
+                out = new FileOutputStream(imFile);
+                bitmap.compress(Bitmap.CompressFormat.PNG,100,out);
+                out.flush();
+                out.close();
+            } catch (java.io.IOException e){
+                e.printStackTrace();
+            }
+            imageView.setImageBitmap(bitmap);
+
         }
-        imageView.setImageBitmap(bitmap);
+
+        else if (requestCode == 3){
+            System.out.print("Wei");
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+           // uploadImage(filePath);
+        }
+
 
     }
 
@@ -85,4 +130,73 @@ public class MainActivity extends AppCompatActivity {
         String formattedDate = s.format(c.getTime());
         return formattedDate;
     }
+
+    private String getPath(Uri uri){
+        Cursor cursor = getContentResolver().query(uri, null,null,null,null);
+        cursor.moveToFirst();
+        String document_id = cursor.getString(0);
+        document_id = document_id.substring(document_id.lastIndexOf(":")+1);
+        cursor.close();
+
+        cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,null,MediaStore.Images.Media._ID + "=?", new String[]{document_id},null);
+        cursor.moveToFirst();
+        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+        cursor.close();
+        return path;
+    }
+
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            //If the user has denied the permission previously your code will come to this block
+            //Here you can explain why you need this permission
+            //Explain here why you need this permission
+        }
+        //And finally ask for the permission
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 123);
+    }
+
+
+    //This method will be called when the user will tap on allow or deny
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        //Checking the request code of our request
+        if (requestCode == 123) {
+
+            //If permission is granted
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Displaying a toast
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                //Displaying another toast if permission is not granted
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void uploadImage(Uri filePath){
+        String name = "Ni Hao";
+        String path = getPath(filePath);
+
+        try {
+            String uploadId = UUID.randomUUID().toString();
+
+            //Creating a multi part request
+            new MultipartUploadRequest(this, uploadId, Upload_URL)
+                    .addFileToUpload(path, "image") //Adding file
+                    .addParameter("name", name) //Adding text parameter to the request
+                    .setNotificationConfig(new UploadNotificationConfig())
+                    .setMaxRetries(2)
+                    .startUpload(); //Starting the upload
+
+        } catch (Exception exc) {
+            Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
     }
